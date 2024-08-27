6869b79124c7933feb71901a765a79b34d165447
Lesnoi Dmytro <dmytrolesnoi@gmail.com>
2024-08-27 15:35:30 +0300

    Task update implemented

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -9,6 +9,8 @@ interface ITaskProps {
 
 const Task = ({task, setTasks}: ITaskProps) => {
     const [updating, setUpdating] = useState<boolean>(false);
+    const [title, setTitle] = useState<string>("");
+    const [description, setDescription] = useState<string>("");
 
     const changeCompleted = () => {
         updateTaskRequest({
@@ -17,12 +19,27 @@ const Task = ({task, setTasks}: ITaskProps) => {
         });
     }
 
+    const updateTask = async () => {
+        await updateTaskRequest({
+            ...task,
+            title,
+            description
+        });
+        setUpdating(false);
+    }
+
     const updateTaskRequest = async (task: ITask) => {
         try {
             const newTask = await axios.put<ITask>(`http://localhost:3000/tasks`, task)
                 .then(res => res.data);
             setTasks(prev => {
-                const index = prev.indexOf(task);
+                let index = -1;
+                for (let i = 0; i< prev.length; i++) {
+                    if (task._id === prev[i]._id) {
+                        index = i;
+                        break;
+                    }
+                }
                 const newTasks = [...prev];
                 newTasks.splice(index, 1, newTask);
                 return newTasks;
@@ -42,14 +59,20 @@ const Task = ({task, setTasks}: ITaskProps) => {
     return (
         <div>
             <h4>Title: </h4>
-            <p>{task.title}</p>
+            {updating ?
+                <input type='text' id='title' placeholder='Title'
+                       value={title} onChange={(e) => setTitle(e.target.value)}/> :
+                <p>{task.title}</p>}
             <h4>Description: </h4>
-            <p>{task.description}</p>
+            {updating ?
+                <input type='text' id='description' placeholder='Description'
+                       value={description} onChange={(e) => setDescription(e.target.value)}/> :
+                <p>{task.description}</p>}
             <h4>Completed: </h4>
             <input type='checkbox' checked={task.isCompleted} onChange={changeCompleted}/>
             <button onClick={() => setUpdating(prev => !prev)}>Update</button>
             <button onClick={deleteTask}>Delete</button>
-            {updating && <button >Save</button>}
+            {updating && <button onClick={updateTask}>Save</button>}
         </div>
     );
 }
